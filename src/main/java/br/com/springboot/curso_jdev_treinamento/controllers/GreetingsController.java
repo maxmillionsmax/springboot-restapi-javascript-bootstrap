32ff5fe3264a6ce536d66069596d86e785bb7d1a
maxmillionsmax <maximilianocaetano@gmail.com>
2022-04-19 18:00:14 -0300

    Criando End-point para salvar no banco

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -56,6 +58,15 @@ public class GreetingsController {
     	List<Usuario> usuarios = usuarioRepository.findAll();
     	
         return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+    }
+    
+    @PostMapping(value = "salvar")/*mapeia url*/
+    @ResponseBody/*descrição da resposta*/
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){/*recebe dados para salvar*/
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+        return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
     }
     
   
